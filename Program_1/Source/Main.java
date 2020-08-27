4b39c38443fa8f8c07ccecc2e787f5779dba5397
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-08-27 21:36:24 +0530

    Add files via upload

---
+++ Program_1/Source/Main.java
@@ -0,0 +1,15 @@
+/*
+    Problem Statement : write a program to print jay ganesh on screen
+*/
+
+package Main;
+import Helper.Helper;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Helper h = new Helper();
+        h.Display();
+    }
+}
