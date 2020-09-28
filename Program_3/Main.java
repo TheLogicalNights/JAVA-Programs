789c7c5a0a11fe6bec2de2f432275fec8863527c
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-09-29 00:37:53 +0530

    Add files via upload

---
+++ Program_3/Main.java
@@ -0,0 +1,33 @@
+/*
+    Problem Statement : Accept two numbers from user and return addition of that numbers
+*/
+
+import java.util.*;
+
+class Numbers
+{
+    int Add(int iNo1,int iNo2)
+    {
+        return iNo1+iNo2;
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int iNo1 = cin.nextInt();
+
+        System.out.println("Enter second number");
+        int iNo2 = cin.nextInt();
+
+        Numbers nobj = new Numbers();
+
+        int iRet =  nobj.Add(iNo1,iNo2);
+
+        System.out.println("Addition is : "+iRet);
+    }
+}
